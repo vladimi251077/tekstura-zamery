8ce2b60ebc6b562210a3eb61f43e828f49238952
vladimi251077 <golovin-vla@yandex.ru>
2026-06-04 09:00:21 +0300

    Use dedicated iOS offline start page

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,31 @@
-const CACHE_VERSION = "tekstura-offline-shell-v20";
+const CACHE_VERSION = "tekstura-offline-shell-v21";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-app-shell`;
-const IOS_START_URLS = [
-  "./",
+const IOS_OFFLINE_START_URLS = [
+  "./offline-start.html",
+  "/offline-start.html",
+  new URL("./offline-start.html", self.registration.scope).href,
+  new URL("/offline-start.html", self.location.origin).href,
+];
+
+const IOS_INDEX_URLS = [
   "./index.html",
-  "/",
   "/index.html",
+  new URL("./index.html", self.registration.scope).href,
+  new URL("/index.html", self.location.origin).href,
+];
+
+const IOS_ROOT_URLS = [
+  "./",
+  "/",
   self.registration.scope,
   new URL("./", self.registration.scope).href,
-  new URL("./index.html", self.registration.scope).href,
+  new URL("/", self.location.origin).href,
+];
+
+const IOS_START_URLS = [
+  ...IOS_OFFLINE_START_URLS,
+  ...IOS_INDEX_URLS,
+  ...IOS_ROOT_URLS,
 ];
 
 const APP_SHELL_URLS = [
@@ -16,8 +34,8 @@ const APP_SHELL_URLS = [
   "./offline-test.html",
   "/styles.css?v=20260518-trash-bulk",
   "./styles.css?v=20260518-trash-bulk",
-  "/app.js?v=20260603-ios-pwa-cold-start-hotfix",
-  "./app.js?v=20260603-ios-pwa-cold-start-hotfix",
+  "/app.js?v=20260604-ios-offline-start",
+  "./app.js?v=20260604-ios-offline-start",
   "/offline-db.js?v=20260517-v4",
   "./offline-db.js?v=20260517-v4",
   "/photo-preview.js?v=20260515-v14",
@@ -36,8 +54,8 @@ const REQUIRED_APP_SHELL_URLS = new Set([
   "./offline-test.html",
   "/styles.css?v=20260518-trash-bulk",
   "./styles.css?v=20260518-trash-bulk",
-  "/app.js?v=20260603-ios-pwa-cold-start-hotfix",
-  "./app.js?v=20260603-ios-pwa-cold-start-hotfix",
+  "/app.js?v=20260604-ios-offline-start",
+  "./app.js?v=20260604-ios-offline-start",
   "/offline-db.js?v=20260517-v4",
   "./offline-db.js?v=20260517-v4",
   "/manifest.webmanifest",
@@ -47,15 +65,9 @@ const REQUIRED_APP_SHELL_URLS = new Set([
 ]);
 
 const NAVIGATION_FALLBACK_URLS = [
-  "./",
-  "./index.html",
-  "/",
-  "/index.html",
-  self.registration.scope,
-  new URL("./", self.registration.scope).href,
-  new URL("./index.html", self.registration.scope).href,
-  new URL("/", self.location.origin).href,
-  new URL("/index.html", self.location.origin).href,
+  ...IOS_OFFLINE_START_URLS,
+  ...IOS_INDEX_URLS,
+  ...IOS_ROOT_URLS,
 ];
 
 const LOCAL_CACHEABLE_DESTINATIONS = new Set(["document", "script", "style", "manifest", "image"]);
@@ -105,11 +117,18 @@ async function cacheShellUrl(cache, url) {
 }
 
 async function cacheIosStartUrls(cache) {
+  const offlineStartRequest = new Request(new URL("./offline-start.html", self.registration.scope).href, { cache: "reload" });
+  const offlineStartResponse = await fetch(offlineStartRequest);
+  if (!offlineStartResponse.ok) throw new Error(`HTTP ${offlineStartResponse.status} ${offlineStartRequest.url}`);
+  for (const url of IOS_OFFLINE_START_URLS) {
+    await cache.put(url, offlineStartResponse.clone());
+  }
+
   const indexRequest = new Request(new URL("./index.html", self.registration.scope).href, { cache: "reload" });
-  const response = await fetch(indexRequest);
-  if (!response.ok) throw new Error(`HTTP ${response.status} ${indexRequest.url}`);
-  for (const url of IOS_START_URLS) {
-    await cache.put(url, response.clone());
+  const indexResponse = await fetch(indexRequest);
+  if (!indexResponse.ok) throw new Error(`HTTP ${indexResponse.status} ${indexRequest.url}`);
+  for (const url of [...IOS_INDEX_URLS, ...IOS_ROOT_URLS]) {
+    await cache.put(url, indexResponse.clone());
   }
 }
 
